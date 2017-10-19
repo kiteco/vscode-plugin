e6f79eaf2932925bfbe8e8ed610b3c43467ef700
abe33 <cedric.nehemie@gmail.com>
2017-10-19 17:01:52 +0200

    Implement download progress in install flow

--- assets/js/install.js
+++ assets/js/install.js
@@ -1,4 +1,19 @@
-window.initInstall = function initInstall() {}
+window.initDownloadProgress = function initDownloadProgress() {
+  const downloadKiteContainer = document.querySelector('.download-kite');
+  const progress = document.querySelector('progress');
+
+  const interval = setInterval(() => {
+    requestGet('/install/progress')
+    .then(ratio => {
+      ratio = parseFloat(ratio);
+      downloadKiteContainer.classList.toggle('hidden', ratio === -1 || ratio === 1);
+      if (ratio !== -1) {
+        progress.value = Math.round(ratio * 100);
+        if (ratio === 1) { clearInterval(interval); }
+      } 
+    });
+  }, 50);
+}
 
 window.submitEvent = function(event) {
   const form = document.querySelector('form');
