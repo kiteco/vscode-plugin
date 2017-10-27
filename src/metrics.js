772ce412eea9b50390bc1a903a2db558a851b2be
abe33 <cedric.nehemie@gmail.com>
2017-10-27 15:41:53 +0200

    Fix metrics name

--- src/metrics.js
+++ src/metrics.js
@@ -54,11 +54,11 @@ function sendFeatureMetric(name) {
 }
 
 function featureRequested(name) {
-  sendFeatureMetric(`atom_${name}_requested`);
+  sendFeatureMetric(`vscode_${name}_requested`);
 }
 
 function featureFulfilled(name) {
-  sendFeatureMetric(`atom_${name}_fulfilled`);
+  sendFeatureMetric(`vscode_${name}_fulfilled`);
 }
 
 function track(event, properties = {}) {
