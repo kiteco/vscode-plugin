eecd09f91e569f9fa9e305f129c0a33382ad1ee7
Marcin Wichary <mwichary@gmail.com>
2018-04-02 12:04:25 -0700

    More fixes.

--- test/section-helpers.js
+++ test/section-helpers.js
@@ -17,7 +17,7 @@ module.exports.hasHeaderSection = (content) => {
 
 module.exports.hasMembersSection = (members, id) => {
   it(`renders a list of the top ${members} modules members`, () => {
-    const section = document.querySelector('section.top-members, section.top-attributes');
+    const section = document.querySelector('section.popular-members');
     expect(section.querySelectorAll('li').length).to.eql(members);
 
     const link = section.querySelector('.more-members')
@@ -31,10 +31,10 @@ module.exports.hasDocsSection = (docs) => {
   it('renders a docs section', () => {
     const section = document.querySelector('.summary');
     const summary = section.querySelector('.description');
-    
+
     expect(section).not.to.be(null);
     expect(summary).not.to.be(null);
-    
+
     expect(section.classList.contains('collapsible')).to.be(true);
     expect(section.classList.contains('collapse')).to.be(true);
     expect(testInternalLinks(stripTBody(summary.innerHTML)))
@@ -45,7 +45,7 @@ module.exports.hasDocsSection = (docs) => {
 function testInternalLinks(html) {
   return html
   .replace(/<a class="internal_link" href="([^"]+)"/g, (m, r) => `<a class="internal_link" href='${r.replace(/&quot;/g, '"')}'`)
-  .replace(/<a href="([^"]+)" class="internal_link"/g, 
+  .replace(/<a href="([^"]+)" class="internal_link"/g,
     (m, r) => `<a href='${r.replace(/&quot;/g, '"')}' class="internal_link"`);
 }
 
@@ -61,7 +61,7 @@ module.exports.hasExamplesSection = (count, id, examples) => {
 
     if (examples.length > count) {
       const link = section.querySelector('.more-examples');
-  
+
       expect(link).not.to.be(null);
       expect(link.href).to.eql(`command:kite.navigate?%22examples-list/${id}%22`);
     }
@@ -80,7 +80,7 @@ module.exports.hasLinksSection = (count, id, links) => {
 
     if (links.length > count) {
       const link = section.querySelector('.more-links');
-  
+
       expect(link).not.to.be(null);
       expect(link.href).to.eql(`command:kite.navigate?%22links-list/${id}%22`);
     }
@@ -89,7 +89,7 @@ module.exports.hasLinksSection = (count, id, links) => {
 
 module.exports.hasArgumentsSection = (functionDetails, language) => {
   const args = functionDetails.parameters;
-  
+
   it('renders a list of the function arguments', () => {
     const section = document.querySelector('section.parameters');
     const dts = section.querySelectorAll('dt');
@@ -109,7 +109,7 @@ module.exports.hasArgumentsSection = (functionDetails, language) => {
   });
 
   switch(language) {
-    case 'python': 
+    case 'python':
       if(functionDetails.language_details.python.kwarg) {
         it('renders a list of the function kwargs', () => {
           const section = document.querySelector('section.kwargs');
@@ -132,37 +132,37 @@ module.exports.hasUsagesSection = (usages) => {
     });
 
     after(() => stub.restore());
-    
+
     it('renders the usages section with its content', () => {
       const section = document.querySelector('section.examples-from-your-code');
-      
+
     });
   });
-  
+
   describe('with a community plan', () => {
     let stubCan, stubTrial;
 
     before(() => {
       stubCan = sinon.stub(Plan, 'can').returns(false);
     });
-    
+
     after(() => stubCan.restore());
-    
+
     describe('with a started trial', () => {
       before(() => {
         stubTrial = sinon.stub(Plan, 'hasStartedTrial').returns(true);
       })
-      
+
       after(() => stubTrial.restore());
 
       it('renders a message to upgrade plan', () => {
         const section = document.querySelector('section.examples-from-your-code');
-  
+
         expect(textContent(section.querySelector('.section-content')))
         .to.eql(`To see ${usages.length} usages, upgrade to Kite Pro, or get Kite Pro for free`);
-        
+
         const links = section.querySelectorAll('a');
-        
+
         expect(links[0].href).to.eql('command:kite.web-url?%22http://localhost:46624/redirect/pro%22');
         expect(links[1].href).to.eql('command:kite.web-url?%22http://localhost:46624/redirect/invite%22');
       });
@@ -172,17 +172,17 @@ module.exports.hasUsagesSection = (usages) => {
       before(() => {
         stubTrial = sinon.stub(Plan, 'hasStartedTrial').returns(false);
       })
-      
+
       after(() => stubTrial.restore());
 
       it('renders a message to upgrade plan', () => {
         const section = document.querySelector('section.examples-from-your-code');
-  
+
         expect(textContent(section.querySelector('.section-content')))
         .to.eql(`To see ${usages.length} usages, start your Kite Pro trial at any time`);
-        
+
         const links = section.querySelectorAll('a');
-        
+
         expect(links[0].href).to.eql('command:kite.web-url?%22http://localhost:46624/redirect/trial%22');
       });
     });
