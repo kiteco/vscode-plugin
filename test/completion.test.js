a2b8feffd8e2bf4722a73200a067963f84f76048
abe33 <cedric.nehemie@gmail.com>
2018-07-30 13:37:44 +0200

    Replace old kite helpers with the one from kite-api

--- test/completion.test.js
+++ test/completion.test.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const expect = require('expect.js');
 const vscode = require('vscode');
-const {fixtureURI, withRoutes, withKiteWhitelistedPaths, fakeResponse, Kite} = require('./helpers');
+const {fixtureURI, Kite} = require('./helpers');
+
+const {withKite, withKitePaths, withKiteRoutes} = require('kite-api/test/helpers/kite');
+const {fakeResponse} = require('kite-api/test/helpers/http');
 
 const KiteCompletionProvider = require('../src/completion');
 
@@ -11,49 +14,51 @@ describe('KiteCompletionProvider', () => {
   beforeEach(() => {
     provider = new KiteCompletionProvider(Kite, true);
   });
-  withKiteWhitelistedPaths([__dirname], () => {
-    describe('when the endpoints returns some completions', () => {
-      withRoutes([
-        [
-          o => /\/clientapi\/editor\/completions/.test(o.path),
-          o => fakeResponse(200, fs.readFileSync(fixtureURI('completions.json').toString()))
-        ]
-      ]);
+  withKite({logged: true}, () => {
+    withKitePaths({whitelist: [__dirname]}, undefined, () => {
+      describe('when the endpoints returns some completions', () => {
+        withKiteRoutes([
+          [
+            o => /\/clientapi\/editor\/completions/.test(o.path),
+            o => fakeResponse(200, fs.readFileSync(fixtureURI('completions.json').toString()))
+          ]
+        ]);
 
-      it('provides them as suggestions ', () => {
-        const uri = vscode.Uri.file(fixtureURI('sample.py'));
+        it('provides them as suggestions ', () => {
+          const uri = vscode.Uri.file(fixtureURI('sample.py'));
 
-        return vscode.workspace.openTextDocument(uri)
-        .then(doc => provider.provideCompletionItems(doc, new vscode.Position(19, 13), null))
-        .then(res => {
-          expect(res.length).to.eql(2);
+          return vscode.workspace.openTextDocument(uri)
+          .then(doc => provider.provideCompletionItems(doc, new vscode.Position(19, 13), null))
+          .then(res => {
+            expect(res.length).to.eql(2);
 
-          expect(res[0].label).to.eql('dumps');
-          expect(res[0].insertText).to.eql('idumps');
-          expect(res[0].sortText).to.eql('0');
+            expect(res[0].label).to.eql('dumps');
+            expect(res[0].insertText).to.eql('idumps');
+            expect(res[0].sortText).to.eql('0');
 
-          expect(res[1].label).to.eql('dump');
-          expect(res[1].insertText).to.eql('idump');
-          expect(res[1].sortText).to.eql('1');
+            expect(res[1].label).to.eql('dump');
+            expect(res[1].insertText).to.eql('idump');
+            expect(res[1].sortText).to.eql('1');
+          });
         });
       });
-    });
 
-    describe('when the endpoint responds with a 404', () => {
-      withRoutes([
-        [
-          o => /\/clientapi\/editor\/completions/.test(o.path),
-          o => fakeResponse(404)
-        ]
-      ]);
+      describe('when the endpoint responds with a 404', () => {
+        withKiteRoutes([
+          [
+            o => /\/clientapi\/editor\/completions/.test(o.path),
+            o => fakeResponse(404)
+          ]
+        ]);
 
-      it('returns null', () => {
-        const uri = vscode.Uri.file(fixtureURI('sample.py'));
+        it('returns null', () => {
+          const uri = vscode.Uri.file(fixtureURI('sample.py'));
 
-        return vscode.workspace.openTextDocument(uri)
-        .then(doc => provider.provideCompletionItems(doc, new vscode.Position(19, 13), null))
-        .then(res => {
-          expect(res).to.eql([]);
+          return vscode.workspace.openTextDocument(uri)
+          .then(doc => provider.provideCompletionItems(doc, new vscode.Position(19, 13), null))
+          .then(res => {
+            expect(res).to.eql([]);
+          });
         });
       });
     });
--- test/signature.test.js
+++ test/signature.test.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const expect = require('expect.js');
 const vscode = require('vscode');
-const {fixtureURI, withRoutes, withKiteWhitelistedPaths, fakeResponse, Kite} = require('./helpers');
+const {fixtureURI, Kite} = require('./helpers');
+
+const {withKite, withKitePaths, withKiteRoutes} = require('kite-api/test/helpers/kite');
+const {fakeResponse} = require('kite-api/test/helpers/http');
+
 
 const KiteSignatureProvider = require('../src/signature');
 
@@ -11,48 +15,50 @@ describe('KiteSignatureProvider', () => {
   beforeEach(() => {
     provider = new KiteSignatureProvider(Kite, true);
   });
-  withKiteWhitelistedPaths([__dirname], () => {
-    describe('for a python function with a signature', () => {
-      withRoutes([
-        [
-          o => /\/clientapi\/editor\/signatures/.test(o.path),
-          o => fakeResponse(200, fs.readFileSync(fixtureURI('plot-signatures.json').toString()))
-        ]
-      ]);
+  withKite({logged: true}, () => {
+    withKitePaths({whitelist: [__dirname]}, undefined, () => {
+      describe('for a python function with a signature', () => {
+        withKiteRoutes([
+          [
+            o => /\/clientapi\/editor\/signatures/.test(o.path),
+            o => fakeResponse(200, fs.readFileSync(fixtureURI('plot-signatures.json').toString()))
+          ]
+        ]);
 
-      it('provides a representation of the function signature', () => {
-        const uri = vscode.Uri.file(fixtureURI('sample.py'));
+        it('provides a representation of the function signature', () => {
+          const uri = vscode.Uri.file(fixtureURI('sample.py'));
 
-        return vscode.workspace.openTextDocument(uri)
-        .then(doc => provider.provideSignatureHelp(doc, new vscode.Position(19, 13), null))
-        .then(res => {
-          expect(res.signatures.length).to.eql(1);
-          expect(res.signatures[0].label).to.eql('plot(x:list|uint64, y:list|str)');
-          expect(res.signatures[0].parameters.length).to.eql(2);
-          expect(res.signatures[0].parameters[0].label).to.eql('x:list|uint64');
-          expect(res.signatures[0].parameters[1].label).to.eql('y:list|str');
-          
-          expect(res.activeParameter).to.eql(1);
-          expect(res.activeSignature).to.eql(0);
+          return vscode.workspace.openTextDocument(uri)
+          .then(doc => provider.provideSignatureHelp(doc, new vscode.Position(19, 13), null))
+          .then(res => {
+            expect(res.signatures.length).to.eql(1);
+            expect(res.signatures[0].label).to.eql('plot(x:list|uint64, y:list|str)');
+            expect(res.signatures[0].parameters.length).to.eql(2);
+            expect(res.signatures[0].parameters[0].label).to.eql('x:list|uint64');
+            expect(res.signatures[0].parameters[1].label).to.eql('y:list|str');
+            
+            expect(res.activeParameter).to.eql(1);
+            expect(res.activeSignature).to.eql(0);
+          });
         });
       });
-    });
 
-    describe('for a python function with no signature', () => {
-      withRoutes([
-        [
-          o => /\/clientapi\/editor\/signatures/.test(o.path),
-          o => fakeResponse(404)
-        ]
-      ]);
+      describe('for a python function with no signature', () => {
+        withKiteRoutes([
+          [
+            o => /\/clientapi\/editor\/signatures/.test(o.path),
+            o => fakeResponse(404)
+          ]
+        ]);
 
-      it('returns null', () => {
-        const uri = vscode.Uri.file(fixtureURI('sample.py'));
+        it('returns null', () => {
+          const uri = vscode.Uri.file(fixtureURI('sample.py'));
 
-        return vscode.workspace.openTextDocument(uri)
-        .then(doc => provider.provideSignatureHelp(doc, new vscode.Position(19, 13), null))
-        .then(res => {
-          expect(res).to.be(null);
+          return vscode.workspace.openTextDocument(uri)
+          .then(doc => provider.provideSignatureHelp(doc, new vscode.Position(19, 13), null))
+          .then(res => {
+            expect(res).to.be(null);
+          });
         });
       });
     });
