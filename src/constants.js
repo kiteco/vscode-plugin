f24293ef514b8ad3a66d259b6a1a197c6c54379a
Tony Cheang <tony@kite.com>
2020-11-02 10:08:35 -0800

    Revert "Revert "Per Filetype Disable (#342)""
    This reverts commit c735ee27614e10814fafa05aa3bd6cc5d83d74a2.

--- src/constants.js
+++ src/constants.js
@@ -1,61 +1,113 @@
 const vscode = require("vscode");
+const path = require("path");
 
-const EVENT_SUPPORT = (fileName) => {
-  const path = require("path");
+// Cache of type: []string
+let ENABLED_AND_SUPPORTED = null;
+// Cache of type: bool
+let PYTHON_ENABLED = null;
+
+function EnabledAndSupported() {
+  if (ENABLED_AND_SUPPORTED === null) {
+    const disabled = vscode.workspace.getConfiguration('kite').completions.disabledFileExtensions;
+    const enabled = SupportedExtensions().filter(ext => !disabled.includes(ext));
+    ENABLED_AND_SUPPORTED = enabled;
+  }
+  return ENABLED_AND_SUPPORTED;
+}
+
+function PythonEnabled() {
+  if (PYTHON_ENABLED === null) {
+    const disabled = vscode.workspace.getConfiguration('kite').completions.disabledFileExtensions;
+    PYTHON_ENABLED = !disabled.includes('.py');
+  }
+  return PYTHON_ENABLED;
+}
+
+function EventSupported(fileName) {
   const fileExt = path.extname(fileName);
-  return SUPPORTED_EXTENSIONS.includes(fileExt);
-};
+  return EnabledAndSupported().includes(fileExt);
+}
 
-const COMPLETIONS_SUPPORT = [
-  { pattern: "**/*.{c,cc,cpp,cs,css,go,h,hpp,html,java,js,jsx,kt,less,m,php,rb,scala,sh,ts,tsx,vue}", scheme: "file" },
-  { pattern: "**/*.{c,cc,cpp,cs,css,go,h,hpp,html,java,js,jsx,kt,less,m,php,rb,scala,sh,ts,tsx,vue}", scheme: "untitled" }
-];
+function CompletionsSupport() {
+  const enabled = EnabledAndSupported().join(',');
+  if (enabled === "") {
+    return [];
+  }
+  return [
+    { pattern: `**/*{${enabled}}`, scheme: "file" },
+    { pattern: `**/*{${enabled}}`, scheme: "untitled" }
+  ];
+}
 
-const FULL_COMPLETIONS_SUPPORT = [
-  { pattern: "**/*.{py}", scheme: "file" },
-  { pattern: "**/*.{py}", scheme: "untitled" }
-];
+function requirePythonEnabled(fn) {
+  return (...args) => {
+    if (!PythonEnabled()) {
+      return [];
+    }
+    return fn(...args);
+  };
+}
 
-const DEFINITIONS_SUPPORT = [
-  { pattern: "**/*.{py}", scheme: "file" },
-  { pattern: "**/*.{py}", scheme: "untitled" }
-];
+function FullCompletionsSupport() {
+  return [
+    { pattern: "**/*.{py}", scheme: "file" },
+    { pattern: "**/*.{py}", scheme: "untitled" }
+  ];
+}
 
-const HOVER_SUPPORT = [
-  { pattern: "**/*.{py}", scheme: "file" },
-  { pattern: "**/*.{py}", scheme: "untitled" }
-];
+function DefinitionsSupport() {
+  return [
+    { pattern: "**/*.{py}", scheme: "file" },
+    { pattern: "**/*.{py}", scheme: "untitled" }
+  ];
+}
 
-const SIGNATURES_SUPPORT = [
-  { pattern: "**/*.{py}", scheme: "file" },
-  { pattern: "**/*.{py}", scheme: "untitled" }
-];
+function HoverSupport() {
+  return [
+    { pattern: "**/*.{py}", scheme: "file" },
+    { pattern: "**/*.{py}", scheme: "untitled" }
+  ];
+}
 
-const SUPPORTED_EXTENSIONS = [
-    ".c",
-    ".cc",
-    ".cpp",
-    ".cs",
-    ".css",
-    ".go",
-    ".h",
-    ".hpp",
-    ".html",
-    ".java",
-    ".js",
-    ".jsx",
-    ".kt",
-    ".less",
-    ".m",
-    ".php",
-    ".py",
-    ".rb",
-    ".scala",
-    ".sh",
-    ".ts",
-    ".tsx",
-    ".vue"
-];
+function SignaturesSupport() {
+  return [
+    { pattern: "**/*.{py}", scheme: "file" },
+    { pattern: "**/*.{py}", scheme: "untitled" }
+  ];
+}
+
+function SupportedExtensions() {
+  return [
+      ".c",
+      ".cc",
+      ".cpp",
+      ".cs",
+      ".css",
+      ".go",
+      ".h",
+      ".hpp",
+      ".html",
+      ".java",
+      ".js",
+      ".jsx",
+      ".kt",
+      ".less",
+      ".m",
+      ".php",
+      ".py",
+      ".rb",
+      ".scala",
+      ".sh",
+      ".ts",
+      ".tsx",
+      ".vue"
+  ];
+}
+
+function IsSupportedFile(fileName) {
+  const fileExt = path.extname(fileName);
+  return SupportedExtensions().includes(fileExt);
+}
 
 const CONNECT_ERROR_LOCKOUT = 15 * 60;
 
@@ -81,16 +133,17 @@ const OFFSET_ENCODING = "utf-16";
 module.exports = {
   ATTEMPTS,
   INTERVAL,
-  EVENT_SUPPORT,
-  COMPLETIONS_SUPPORT,
-  FULL_COMPLETIONS_SUPPORT,
-  DEFINITIONS_SUPPORT,
-  HOVER_SUPPORT,
-  SIGNATURES_SUPPORT,
+  EventSupported,
+  CompletionsSupport,
+  FullCompletionsSupport: requirePythonEnabled(FullCompletionsSupport),
+  DefinitionsSupport: requirePythonEnabled(DefinitionsSupport),
+  HoverSupport: requirePythonEnabled(HoverSupport),
+  SignaturesSupport: requirePythonEnabled(SignaturesSupport),
+  SupportedExtensions,
+  IsSupportedFile,
   CONNECT_ERROR_LOCKOUT,
   ERROR_COLOR,
   WARNING_COLOR,
-  SUPPORTED_EXTENSIONS,
   KITE_BRANDING,
   OFFSET_ENCODING
 };
