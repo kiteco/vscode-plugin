63f069f55b818a43c9163500fc0f0155e1fc087d
Tony Cheang <tony@kite.com>
2021-01-05 15:40:47 -0800

    change: white logo to light-blue

--- src/codenav-decoration.ts
+++ src/codenav-decoration.ts
@@ -87,7 +87,7 @@ export default class KiteRelatedCodeDecorationsProvider {
   }
 
   private hoverMessage(hover: string): MarkdownString {
-    const logo = path.join(extensions.getExtension("kiteco.kite").extensionPath , "/dist/assets/images/logo-white.svg");
+    const logo = path.join(extensions.getExtension("kiteco.kite").extensionPath , "/dist/assets/images/logo-light-blue.svg");
     const md = new MarkdownString(`![KiteIcon](${logo}|height=10) [${hover}](command:kite.related-code-from-line)`);
     // Must mark as trusted to run commands in MarkdownStrings
     md.isTrusted = true;
