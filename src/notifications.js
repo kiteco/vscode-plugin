883ef9a3ba71aedf3d12489d7fcaef27072a4d00
Edward Zhao <ed@kite.com>
2021-02-17 11:15:09 -0800

    Add prompt before installation

--- src/notifications.js
+++ src/notifications.js
@@ -99,7 +99,7 @@ export default class NotificationsManager {
       .then(hasDone => !hasDone && openKiteTutorial('python'));
   }
 
-  static showKiteInstallNotification() {
+  static showKiteInstallNotification(install) {
     metrics.track("vscode_kite_installer_notification_shown");
     vscode.window
       .showInformationMessage(
@@ -110,8 +110,12 @@ export default class NotificationsManager {
       .then(item => {
         switch (item) {
           case "Install":
-            open("https://www.kite.com/install/?utm_medium=editor&utm_source=vscode");
-            metrics.track("vscode_kite_installer_github_link_clicked");
+            if (!install) {
+              open("https://www.kite.com/install/?utm_medium=editor&utm_source=vscode");
+              metrics.track("vscode_kite_installer_github_link_clicked")
+            } else {
+              install();
+            }
             break;
           case "Learn More":
             open("https://www.kite.com/copilot/");
@@ -139,8 +143,8 @@ export default class NotificationsManager {
       });
   }
 
-  static showKiteInstallErrorNotification() {
-    metrics.track("vscode_kite_downloading_failed_notification_shown");
+  static showKiteInstallErrorNotification(error) {
+    metrics.track("vscode_kite_downloading_failed_notification_shown", { error });
     vscode.window
       .showErrorMessage(
         "There was an error installing the Kite Copilot, which is required for Kite to provide completions and documentation. Please install it to use Kite.",
